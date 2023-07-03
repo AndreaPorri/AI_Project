1458bbabaa811e6f4c3d42d100fd3d770c224726
AndreaPorri <105643996+AndreaPorri@users.noreply.github.com>
2023-07-03 16:57:58 +0200

    Add files via upload

--- dataset_function.py
+++ dataset_function.py
@@ -1,79 +1,140 @@
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
+from main import load_csv_file
 
-def load_data_from_file(file):
-    """Load data from the Boston Housing data file, creating the X and y tensors.
+def dataset_reduction(dataframe, colonna_input, colonna_target):
+    # Seleziona le colonne 13 e 14 dal dataframe
+    dataframe_reduced = dataframe.loc[:, [f"{colonna_input}",f"{colonna_target}"]]
+    
+    return dataframe_reduced
 
-    Args:
-        file: the name of the CSV file with the data.
 
-    Returns:
-        The NumPy tensors X,y (data and targets).
-    """
 
-    # loading data from file (regression targets are on the last column)
-    Xy = np.loadtxt(file, dtype=np.float32)
+def create_file_csv(dataframe):
+    #Salvo il mio dataframe in un nuovo file .csv
+    dataframe.to_csv('C:/Users/andre/OneDrive/Desktop/MAGISTRALE/AI_Project/Dataset/dataset_reduced.csv', index=False)
 
-    # splitting data and targets
-    X = Xy[:, 0:-1]
-    y = Xy[:, -1].reshape((X.shape[0], 1))
+
+def check_dataset_format(dataframe):
+    ### Rimuovo i record con valori NaN o null o non validi ###
+    #Creando una copia esplicita del DataFrame, puoi assicurarti che le modifiche vengano apportate sull'oggetto originale e non su una copia, evitando così l'errore "SettingWithCopyWarning"
+    dataframe_copy = dataframe.copy()
+    #rimozione dati non validi
+    dataframe_copy.dropna(inplace=True) 
+
+    ### Controllo che il formato delle colonne restanti sia corretto ###
+    #Definisco il formato desiderato utilizzando un'espressione regolare
+    formato_desiderato = r'^(-?\d+,\d+)$'
+
+    # Verifico che tutti i valori delle due colonne rispettino il formato desiderato
+    is_valid = dataframe_copy["T"].str.match(formato_desiderato) & dataframe_copy["RH"].str.match(formato_desiderato)
+
+    # Rimuovo i record che non rispettano il formato desiderato
+    dataframe_copy = dataframe_copy[is_valid]
+
+
+    ### Converto le colonne nel datatype float ###
+    dataframe_copy.loc[:, "T"] = dataframe_copy.loc[:, "T"].str.replace(',', '.').astype(float)
+    dataframe_copy.loc[:, "RH"] = dataframe_copy.loc[:,"RH"].str.replace(',', '.').astype(float)
+
+
+    ### Verifico che i dati ricadano in un range ragionevole, altrimenti verranno eliminati ###
+    # Verifico che la prima colonna sia compresa tra -100° e 60°
+    condizione1 = (dataframe_copy["T"] >= -100) & (dataframe_copy["T"] <= 60)
+
+    # Verifico che la seconda colonna sia compresa tra 0% e 100%
+    condizione2 = (dataframe_copy["RH"] >= 0) & (dataframe_copy["RH"] <= 100)
+
+    # Rimuovo i record che non soddisfano entrambe le condizioni
+    dataframe = dataframe_copy[condizione1 & condizione2]
+
+    #Rindicizzo le righe sennò avrei dei "buchi" nelle indicizzazioni dovuto al fatto che ho droppato dei record
+    dataframe.reset_index(drop=True, inplace=True)
+
+    return dataframe
+    
+
+
+def load_data_from_file(filename):
+    # Load data dal file .csv che abbiamo appositamente preprocessato
+    data = load_csv_file(filename)
+
+    # splitting data in inputs and targets
+    X = data.iloc[:, 0].to_numpy().reshape((-1, 1)).astype(np.float32) 
+    y = data.iloc[:, 1].to_numpy().reshape((-1, 1)).astype(np.float32)
+
     return X, y
 
-def create_splits_unbalanced(X, y, train_val_fractions=(0.5, 0.25), randomize=True):
-    """Creates (randomized) training, validation, test data splits.
+def create_splits_unbalanced(X, y, train_frac, valid_frac, randomize=True):
+    """
+    Creates (randomized) training, validation, test data splits.
 
     Args:
         X: dataset (one example per row).
         y: ground truth labels (vector).
-        train_val_fractions: the fraction of data to be used in the training and validation sets, respectively.
+        train_frac: the fraction of data to be used in the training set.
+        valid_frac: the fraction of data to be used in the validation set.
         randomize (optional): randomize the data before splitting them.
 
     Returns:
-        The NumPy tensors X,y (data and targets) for training, validation and test sets, respectively.
+        The NumPy tensors X,y (data and targets) for training, validation, and test sets, respectively.
     """
 
-    # randomizing data
-    n = X.shape[0]
+    # Randomize data
     if randomize:
-        indices = np.arange(n)
-        np.random.shuffle(indices)
-        X = X[indices, :]
+        indices = np.random.permutation(X.shape[0])
+        X = X[indices]
         y = y[indices]
 
-    # collecting indices of the examples that will fall into the training, validation and test sets
+    # Calculate split sizes
     n = X.shape[0]
-    a = round(n * train_val_fractions[0])
-    b = a + round(n * train_val_fractions[1])
+    train_size = int(n * train_frac)
+    valid_size = int(n * valid_frac)
 
-    train_set_indices = np.arange(0, a)
-    val_set_indices = np.arange(a, b)
-    test_set_indices = np.arange(b, n)
+    # Split data
+    X_train, y_train = X[:train_size], y[:train_size]
+    X_val, y_val = X[train_size:train_size+valid_size], y[train_size:train_size+valid_size]
+    X_test, y_test = X[train_size+valid_size:], y[train_size+valid_size:]
 
-    # splitting into training, validation and test sets
-    X_train = torch.from_numpy(X[train_set_indices, :])
-    y_train = torch.from_numpy(y[train_set_indices])
-    X_val = torch.from_numpy(X[val_set_indices, :])
-    y_val = torch.from_numpy(y[val_set_indices])
-    X_test = torch.from_numpy(X[test_set_indices, :])
-    y_test = torch.from_numpy(y[test_set_indices])
+    return torch.from_numpy(X_train), torch.from_numpy(y_train), torch.from_numpy(X_val), torch.from_numpy(y_val), torch.from_numpy(X_test), torch.from_numpy(y_test)
 
-    return X_train, y_train, X_val, y_val, X_test, y_test
 
-def normalize_data(X, mean=None, std=None):
-    """Normalize the data to a given mean and standard deviation (or to null-mean and unit-std).
 
-    Args:
-        X: the data to be normalized (one-example-per-row).
-        mean (optional): the array with the means to be used in the normalization.
-        std (optional): the array with the standard deviations to be used in the normalization.
-
-    Returns:
-        The normalized dataset and the arrays with the means and standard deviations that were used.
-    """
-
+def normalize_input_data(X, mean=None, std=None):
+    #Calcolo della media e deviazione standard
     if mean is None:
-        mean = torch.mean(X, dim=0)
+            mean = X.mean()
     if std is None:
-        std = torch.std(X, dim=0)
-    return (X - mean) / std, mean, std
+           std = X.std()
+    # Adjust the range to -1 to 1
+    min_val = X.min()
+    max_val = X.max()
+    normalized_X = 2 * (X - min_val) / (max_val - min_val) - 1
+
+    return normalized_X, mean, std, max_val, min_val
+
+def normalize_input_data_100(X, mean=None, std=None):
+    #Calcolo della media e deviazione standard
+    if mean is None:
+            mean = X.mean()
+    if std is None:
+           std = X.std()
+    # Adjust the range to -1 to 1
+    min_val = -100
+    max_val = 100
+    normalized_X = 2 * (X - min_val) / (max_val - min_val) - 1
+
+    return normalized_X, mean, std, max_val, min_val
+
+
+def normalize_output_data(y, min_val=None, max_val=None):
+    #Calcolo del minimo e massimo
+    if min_val is None:
+        min_val = X.min()
+    if max_val is None:
+        max_val = X.max()
+    normalized_X = (X - min_val) / (max_val - min_val)
+    return normalized_X
+
+
+
---
+++ main.py
@@ -0,0 +1,96 @@
+import pandas as pd
+from dataset_function import *
+import torch
+import torch.nn as nn
+
+
+
+
+def load_csv_file(filename):
+    dataframe = pd.read_csv(filename)
+    return dataframe
+
+def check_dataset(dataframe):
+    #Numero righe e colonne dataset
+    print(dataframe.shape)
+    #Lista nomi colonne
+    lista_colonne= list(dataframe.columns)
+    print(f"Nomi delle colonne 1:{lista_colonne}\n")
+    #Stampa delle prime 10 righe dei dataset
+    print(f"Dataset first lines:\n{dataframe.head(10)}\n\n")  
+
+
+if __name__ == "__main__":
+
+                                    ### PREPROCESS DATASET ###
+
+    #Caricamento dataframe da file .csv
+    filename = "C:/Users/andre/OneDrive/Desktop/MAGISTRALE/AI_Project/Dataset/AirQualityUCI.csv"
+    dataframe = pd.read_csv(filename, sep=";")
+    #print(dataframe.columns)
+
+    #Dataset check caratteristiche:
+    #check_dataset(dataframe)
+
+    #Ridimensionamento dataset
+    dataset_reduced = dataset_reduction(dataframe,"T","RH")
+    #print('Il dataset senza le eliminazioni è:\n ',dataset_reduced)
+    #print('la dimensionalità è: ',dataset_reduced.shape)
+    
+    #Pulitura dataset ridotto
+    dataset_reduced = check_dataset_format(dataset_reduced)
+    #print('Il dataset con le eliminazioni è:\n ',dataset_reduced)
+    #print('la dimensionalità è: ',dataset_reduced.shape)
+    
+    #Device configuration
+    my_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+                        ### DEFINIZIONE DELLA MLP ###
+
+    #Carichiamo i dati dal nuovo file .csv e dividiamo in input e output
+    data_X, data_y = load_data_from_file("C:/Users/andre/OneDrive/Desktop/MAGISTRALE/AI_Project/Dataset/dataset_reduced.csv")
+    
+    #Splitting data in training, validation e test sets
+    data_X_train, data_y_train, data_X_val, data_y_val, data_X_test, data_y_test = create_splits_unbalanced(data_X, data_y, 0.7, 0.15)
+    '''
+    ### CONTEGGIO NEGATIVI PRE NORMALIZZAZIONE ###
+    # Converti il tensore Torch in un tensore booleano con valori True per i numeri negativi
+    negative_mask = (data_X_train < 0)
+
+    # Conta il numero di valori True nel tensore booleano
+    num_negatives = torch.sum(negative_mask).item()
+
+    print("Numero di numeri negativi nel tensore:", num_negatives)
+    '''    
+    # normalizing data input
+    data_X_train, m, s, max, min= normalize_input_data(data_X_train)
+    data_X_val, _, _, _, _ = normalize_input_data(data_X_val, m, s)
+    data_X_test, _, _, _, _ = normalize_input_data(data_X_test, m, s)
+
+    #Stampe forma dati
+    #print("DATI:\n",data_X_train)
+    #print(f"MEDIA:{m}, STD:{s}, MAX:{max}, MIN:{min}")
+    
+    '''
+    ### CONTEGGIO NEGATIVI POST NORMALIZZAZIONE + DOMANDA ###
+    # Converti il tensore Torch in un tensore booleano con valori True per i numeri negativi
+    negative_mask = (data_X_train < 0)
+
+    # Conta il numero di valori True nel tensore booleano
+    num_negatives = torch.sum(negative_mask).item()
+
+    print("Numero di numeri negativi nel tensore:", num_negatives)
+    
+    #Domanda: è normale che io abbia più numeri negativi post normalizzazione? In teoria questa dovrebbe mantenere la distribuzione relativa dei dati nel proprio intervallo.
+    #        Oppure conviene fissare max = 100 e min = -100 e quindi viene una distribuzione sbilanciata ma che mantiene i segni? normalize_input_data_100
+    '''
+    #Ensure that we keep track of the mean and std used to normalize the data
+    torch.save([m, s], 'C:/Users/andre/OneDrive/Desktop/MAGISTRALE/AI_Project/Dataset/normalizers.pth')
+
+    
+ 
+    
+    
+
+      
+   
